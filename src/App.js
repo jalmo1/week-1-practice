1657123d310f91e88679d1fde7d8c17b51b2b340
Joshua Almodovar <joshuarosamktp4@gmail.com>
2022-12-23 12:17:54 -0500

    add files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Navigation from "./Components/Navigation";
+
+
+function App() {
+  return (
+    <main>
+      <Navigation />
+    </main>
+  );
+}
+export default App;
---
+++ src/Components/Navigation.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Navigation() {
+  return (
+    <section>
+      <nav>
+        <ul className="py-10 flex justify-center">
+          <li>
+            <a>Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </section>
+  )
+}
+
+export default Navigation;
